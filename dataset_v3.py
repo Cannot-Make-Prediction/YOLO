66d8ce81ae14656f7aa136c2fe45feeccdae203c
Cannot-Make-Prediction <78393339+Cannot-Make-Prediction@users.noreply.github.com>
2021-05-13 11:49:14 -0400

    Update dataset_v3.py

--- dataset_v3.py
+++ dataset_v3.py
@@ -1,13 +1,8 @@
-"""
-Creates a Pytorch dataset to load the Pascal VOC & MS COCO datasets
-"""
-
 import config
 import numpy as np
 import os
 import pandas as pd
 import torch
-
 from PIL import Image, ImageFile
 from torch.utils.data import Dataset, DataLoader
 from utils import (
@@ -16,9 +11,7 @@ from utils import (
     non_max_suppression as nms,
     plot_image
 )
-
 ImageFile.LOAD_TRUNCATED_IMAGES = True
-
 class YOLODataset(Dataset):
     def __init__(
         self,
@@ -56,8 +49,6 @@ class YOLODataset(Dataset):
             augmentations = self.transform(image=image, bboxes=bboxes)
             image = augmentations["image"]
             bboxes = augmentations["bboxes"]
-
-        # Below assumes 3 scale predictions (as paper) and same num of anchors per scale
         targets = [torch.zeros((self.num_anchors // 3, S, S, 6)) for S in self.S]
         for box in bboxes:
             iou_anchors = iou(torch.tensor(box[2:4]), self.anchors)
@@ -88,40 +79,5 @@ class YOLODataset(Dataset):
                     targets[scale_idx][anchor_on_scale, i, j, 0] = -1  # ignore prediction
 
         return image, tuple(targets)
-
-
-def test():
-    anchors = config.ANCHORS
-
-    transform = config.test_transforms
-
-    dataset = YOLODataset(
-        "COCO/train.csv",
-        "COCO/images/images/",
-        "COCO/labels/labels_new/",
-        S=[13, 26, 52],
-        anchors=anchors,
-        transform=transform,
-    )
-    S = [13, 26, 52]
-    scaled_anchors = torch.tensor(anchors) / (
-        1 / torch.tensor(S).unsqueeze(1).unsqueeze(1).repeat(1, 3, 2)
-    )
-    loader = DataLoader(dataset=dataset, batch_size=1, shuffle=True)
-    for x, y in loader:
-        boxes = []
-
-        for i in range(y[0].shape[1]):
-            anchor = scaled_anchors[i]
-            print(anchor.shape)
-            print(y[i].shape)
-            boxes += cells_to_bboxes(
-                y[i], is_preds=False, S=y[i].shape[2], anchors=anchor
-            )[0]
-        boxes = nms(boxes, iou_threshold=1, threshold=0.7, box_format="midpoint")
-        print(boxes)
-        plot_image(x[0].permute(1, 2, 0).to("cpu"), boxes)
-
-
 if __name__ == "__main__":
     test()
